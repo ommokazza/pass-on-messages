a7da3803fc09d6631b54520c23acba50d60ac245
ommokazza <ommokazza@gmail.com>
2025-10-01 21:24:45 +0900

    Version Up 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.ommoks.azza.android.app.passonnotifications"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
