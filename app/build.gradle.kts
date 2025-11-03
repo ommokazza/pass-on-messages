f5b67c9ee1ebba78400dc0907b1e1bfbb5c66b35
Jiwoong Yoo <ommokazza@gmail.com>
2025-11-03 23:55:54 +0900

    Version Up - 0.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.ommoks.azza.android.app.pass_on_messages"
         minSdk = 29
         targetSdk = 36
-        versionCode = 900
-        versionName = "0.9.0"
+        versionCode = 902
+        versionName = "0.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
